659840340689aa74b32d6a40365127fbcfc03665
Michael Tomcal <mtomcal@cbtnuggets.com>
2015-11-18 11:15:57 -0800

    Added some bootstrap styles

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import React from 'react';
 export default React.createClass({
     render() {
         return (
-            <div className="row">
-                <div className="col-sm-4">
-                    <h1>Project</h1>
+            <div>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a href="#" className="navbar-brand">React Rnd</a>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <h1>Project</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
